f316aaaf8f0b51cc8eb6e4874cd1cd10872bd88d
mohdadil82 <49994620+mohdadil82@users.noreply.github.com>
2023-12-21 11:51:58 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ variable "AWS_REGION" {
 provider "aws" {
     region = "${var.AWS_REGION}"
 }
-resource “aws_vpc” “prod-vpc” {
-    cidr_block = “30.0.0.0/16”
-    enable_dns_support = “true” #gives you an internal domain name
-    enable_dns_hostnames = “true” #gives you an internal host name
-    enable_classiclink = “false”
-    instance_tenancy = “default”    
+resource "aws_vpc" "prod-vpc" {
+    cidr_block = "30.0.0.0/16"
+    enable_dns_support = "true" #gives you an internal domain name
+    enable_dns_hostnames = "true" #gives you an internal host name
+    enable_classiclink = "false"
+    instance_tenancy = "default"    
     
     tags {
-        Name = “prod-vpc”
+        Name = "prod-vpc"
     }
 }
