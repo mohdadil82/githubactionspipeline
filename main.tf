9100e884aefd62337ff8e53831d00bc696b52281
mohdadil82 <49994620+mohdadil82@users.noreply.github.com>
2023-12-21 11:53:57 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
-variable "AWS_REGION" {    
-    default = "eu-west-2"
-}
 provider "aws" {
-    region = "${var.AWS_REGION}"
-}
-resource "aws_vpc" "prod-vpc" {
-    cidr_block = "30.0.0.0/16"
-    enable_dns_support = "true" #gives you an internal domain name
-    enable_dns_hostnames = "true" #gives you an internal host name
-    enable_classiclink = "false"
-    instance_tenancy = "default"    
+  region = "eu-west-1"
+  
     
-    tags {
-        Name = "prod-vpc"
-    }
+  }
+}
+
+
+resource "aws_vpc" "main" {
+ cidr_block = "30.0.0.0/16"
+ 
+ tags = {
+   Name = "Project VPC"
+ }
 }
